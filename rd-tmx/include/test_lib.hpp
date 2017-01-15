9c3e3a5298d9b93dd3f088168adbcc68eb5541ba
hanselrd <hansel809@hotmail.com>
2017-01-15 16:04:09 -0500

    Added test folder

---
+++ rd-tmx/include/test_lib.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace rd {
+	namespace tmx {
+		namespace test {
+			void run();
+		}
+	}
+}
---
+++ test/src/main.cpp
@@ -0,0 +1,6 @@
+#include "test_lib.hpp"
+
+int main(int argc, char** argv) {
+	rd::tmx::test::run();
+	return 0;
+}
